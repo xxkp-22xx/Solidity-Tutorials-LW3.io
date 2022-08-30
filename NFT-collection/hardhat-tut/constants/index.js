63628c919072b7067a910681099d363bbd810151
xxkp-22xx <khwaeeshpatel007@gmail.com>
2022-08-30 05:17:17 -0700

    addr update New

--- NFT-collection/hardhat-tut/constants/index.js
+++ NFT-collection/hardhat-tut/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x4c4691E83DC76eaE93D017A97355c336073879E5";
+const WHITELIST_CONTRACT_ADDRESS = "0x3A3d4E220F6C94aEaAc6aD43DD1DfAd2b5F4f9b0";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-lovat-six.vercel.app/api/";
 
